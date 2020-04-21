bb2ff5632bb9dbb522c8a00386e6e52c53a41cbe
Jonathan Thamrin <jthamrin.dev@gmail.com>
2020-04-22 10:20:11 +1200

    v0.2 - added Google Colab option

--- alexnet.py
+++ alexnet.py
@@ -2,9 +2,20 @@
 import wget
 import zipfile
 
+################# GOOGLE COLAB #################
+running_on_google_colab = False
+
+if running_on_google_colab:
+   file_path = '/flower_data.zip'
+   extract_to = ''
+else:
+   file_path = '/content/flower_data.zip'
+   extract_to = '/content'
+################################################
+
 # Data Files and Labels
 wget.download('https://s3.amazonaws.com/content.udacity-data.com/courses/nd188/flower_data.zip')
 wget.download('https://raw.githubusercontent.com/udacity/pytorch_challenge/master/cat_to_name.json')
 
-with zipfile.ZipFile('/content/flower_data.zip', 'r') as zip_ref:
-    zip_ref.extractall('/content')
+with zipfile.ZipFile(file_path, 'r') as zip_ref:
+   zip_ref.extractall(extract_to)
