5cf6830bd7af9d0ba7f92c18fa9cde83505f4cc2
jthamrin-dev <jthamrin.dev@gmail.com>
2020-04-23 20:32:11 +1200

    v1.0 - test, validate functions added

--- main.py
+++ main.py
@@ -22,38 +22,57 @@ def imsave(img):
    im.save("./results/your_file.jpeg")
 
 # Training Function.
-def train(model, device, train_loader, optimizer, epoch):
+def train(model, device, train_loader, validate_loader, optimizer, epoch):
    model.train()
-   for batch_index, (inputs, labels) in enumerate(train_loader):
-      data, target = data.to(device), target.to(device)
-      # zero the parameter gradients
+   for index, (inputs, labels) in enumerate(train_loader):
+      # inputs = batch of samples (64) || index = batch index (1)
+      inputs, labels = inputs.to(device), labels.to(device)
       optimizer.zero_grad()
-      # forward + backward + optimize
-      output = model(data.view(-1,28*28))
-      loss = F.nll_loss(output, target)
-      loss.backward(); optimizer.step()
-      if batch_idx % log_interval == 0:
-         print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-               epoch, batch_idx * len(data), len(train_loader.dataset),
-               100. * batch_idx / len(train_loader), loss.item()))
+      output = model.forward(inputs)
+      loss = F.nll_loss(output, labels)
+      loss.backward()
+      optimizer.step()
 
-# Validation Function.
-def validate():
+      print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+         epoch, index*len(inputs), len(train_loader.dataset), 
+         100. * index / len(train_loader), loss.item()))
 
-# Testing Function.
-def test():
+      # Validation
+      # valid_test(model, device, validate_loader, 1)
 
+# Validation and Testing Function.
+def valid_test(model, device, valid_test_loader, valid):
+   model.eval()
+   loss = 0; accuracy = 0
+   with torch.no_grad():
+      for images, labels in valid_test_loader:
+         images, labels = images.to(device), labels.to(device)
+         output = model.forward(images)
+         loss += F.nll_loss(output, labels, reduction='sum').item()  # loss is summed before adding to loss
+         pred = output.argmax(dim=1, keepdim=True)  # index of the max log-probability
+         accuracy += pred.eq(labels.view_as(pred)).sum().item()
+
+   loss /= len(valid_test_loader.dataset)
+
+   if valid:
+      word = 'Validate'
+   else:
+      word = 'Test'
+
+   print('\n{} set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+         word, loss, accuracy, len(valid_test_loader.dataset),
+         100. * accuracy / len(valid_test_loader.dataset)))
 
 def main():
    # VARIABLES
-   epochs = 10
+   epochs = 2
    gamma = 0.7
    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
    print(f'Device selected: {str(device).upper()}')
 
    # GOOGLE COLAB
-   running_on_google_colab = False
-   files_downloaded = True
+   running_on_google_colab = True
+   files_downloaded = False
 
    if running_on_google_colab:
       file_path = '/content/flower_data.tar.gz'
@@ -68,6 +87,10 @@ def main():
       wget.download('https://raw.githubusercontent.com/udacity/pytorch_challenge/master/cat_to_name.json')
       shutil.unpack_archive(file_path, extract_to)
       os.remove(file_path)
+   
+   # MAPPING CATEGORY LABELS
+   with open('./cat_to_name.json', 'r') as ctn:
+      cat_to_name = json.load(ctn)
 
    # DEFINING TRANSFORMS
    train_transform = transforms.Compose([
@@ -93,13 +116,24 @@ def main():
       ])
 
    # Prepares and Loads Training, Validation and Testing Data.
-   train_data = datasets.ImageFolder(data+'/train', transform=train_transform)
-   validate_data = datasets.ImageFolder(data+'/valid', transform=validate_test_transform)
-   test_data = datasets.ImageFolder(data+'/test', transform=validate_test_transform)
+   train_data = datasets.ImageFolder(extract_to+'/train', transform=train_transform)
+   validate_data = datasets.ImageFolder(extract_to+'/valid', transform=validate_test_transform)
+   test_data = datasets.ImageFolder(extract_to+'/test', transform=validate_test_transform)
 
-   # MAPPING CATEGORY LABELS
-   with open('./cat_to_name.json', 'r') as ctn:
-      cat_to_name = json.load(ctn)
+   # Defining the Dataloaders using Datasets.
+   train_loader = torch.utils.data.DataLoader(train_data, batch_size=72, shuffle=True)
+   validate_loader = torch.utils.data.DataLoader(validate_data, batch_size=419, shuffle=False)
+   test_loader = torch.utils.data.DataLoader(test_data, batch_size=273, shuffle=False)
+
+   model = AlexNet().to(device)
+
+   optimizer = optim.Adam(model.classifier.parameters(), lr=0.001)
+   #scheduler = StepLR(optimizer, step_size=1, gamma=gamma)
+
+   for epoch in range(1, epochs+1):
+      train(model, device, train_loader, validate_loader, optimizer, epoch)
+      valid_test(model, device, test_loader, 0)
+      #scheduler.step()
 
 if __name__ == '__main__':
    main()
