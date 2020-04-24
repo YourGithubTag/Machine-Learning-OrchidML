141a9c02cf933a619abb0900dece77b9eb9d0b69
Jonathan Thamrin <jthamrin.dev@gmail.com>
2020-04-24 22:18:06 +1200

    removed unnecessary avg pooling

--- model/alexnet.py
+++ model/alexnet.py
@@ -1,9 +1,10 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class AlexNet(nn.Module):
 
-   def __init__(self, num_classes=1000):
+   def __init__(self):
       super(AlexNet, self).__init__()
       self.features = nn.Sequential(
          nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
@@ -20,15 +21,15 @@ class AlexNet(nn.Module):
          nn.ReLU(inplace=True),
          nn.MaxPool2d(kernel_size=3, stride=2),
       )
-      self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+      
       self.classifier = nn.Sequential(
          nn.Dropout(),
          nn.Linear(256 * 6 * 6, 4096),
-         nn.ReLU(inplace=True),
+         nn.ReLU(),
          nn.Dropout(),
          nn.Linear(4096, 4096),
-         nn.ReLU(inplace=True),
-         nn.Linear(4096, num_classes),
+         nn.ReLU(),
+         nn.Linear(4096, 102)
       )
 
    # Since this NN is inheriting the nn.Module, it will have forward() and backward() methods.
@@ -37,7 +38,6 @@ class AlexNet(nn.Module):
    # In this case, forward() link all the layers together.
    def forward(self, x):
       x = self.features(x)
-      x = self.avgpool(x)
       x = torch.flatten(x, 1)
       x = self.classifier(x)
       return x
