9f12d628c211383b37dcd3fdabadae3075835cad
Jonathan Thamrin <jonathanthamrin@Jonathans-MacBook-Pro.local>
2020-04-27 17:19:25 +1200

    fixed transform size; updated dataset & batch size

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import torchvision
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
 import torch.nn.functional as F
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
@@ -55,9 +57,6 @@ def train(model, device, train_loader, validate_loader, optimizer, epoch):
          epoch, index*len(inputs), len(train_loader.dataset), 
          100. * index / len(train_loader), loss.item()))
 
-      # Validation
-      # valid_test(model, device, validate_loader, 1)
-
 # Validation and Testing Function.
 def valid_test(model, device, valid_test_loader, valid):
    model.eval()
@@ -83,26 +82,26 @@ def valid_test(model, device, valid_test_loader, valid):
 
 def main():
    # VARIABLES
-   epochs = 10
-   gamma = 0.7
+   epochs = 14
    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
    print(f'Device selected: {str(device).upper()}')
+   torch.manual_seed(1)
 
    # GOOGLE COLAB
-   running_on_google_colab = True
+   running_on_google_colab = False
    files_downloaded = True
 
    if running_on_google_colab:
-      file_path = '/content/flower_data.tar.gz'
+      file_path = '/content/17Flowers.zip'
       extract_to = '/content/flower_data'
    else:
-      file_path = './flower_data.tar.gz'
-      extract_to = './flower_data'
+      file_path = './17Flowers.zip'
+      extract_to = '../flower_data'
 
    # DOWNLOAD DATA
    if not files_downloaded:
-      wget.download('https://s3.amazonaws.com/content.udacity-data.com/nd089/flower_data.tar.gz')
-      wget.download('https://raw.githubusercontent.com/udacity/pytorch_challenge/master/cat_to_name.json')
+      wget.download('https://dl.dropboxusercontent.com/s/itlaky1ssv8590j/17Flowers.zip')
+      wget.download('https://dl.dropboxusercontent.com/s/rwc40rv1r79tl18/cat_to_name.json')
       shutil.unpack_archive(file_path, extract_to)
       os.remove(file_path)
       print('Files Downloaded...')
@@ -120,20 +119,18 @@ def main():
          transforms.RandomVerticalFlip(p=0.5),
          transforms.RandomRotation(180),
          ]),
-      # Crops image to .
-      transforms.Resize(500),
-      transforms.CenterCrop(384),
+      # Crops image to 227x227.
+      transforms.Resize(256),
+      transforms.CenterCrop(227),
       # Converts to Tensor.
-      transforms.ToTensor(),
-      transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+      transforms.ToTensor()            #transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
       ])
 
    validate_test_transform = transforms.Compose([
-      # Crops image to  from centre.
-      transforms.Resize(500),
-      transforms.CenterCrop(384),
-      transforms.ToTensor(),
-      transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+      # Crops image to 227x227 from centre.
+      transforms.Resize(256),
+      transforms.CenterCrop(227),
+      transforms.ToTensor()            #transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
       ])
 
    # Prepares and Loads Training, Validation and Testing Data.
@@ -150,19 +147,17 @@ def main():
    
 
    # Defining the Dataloaders using Datasets.
-   train_loader = torch.utils.data.DataLoader(train_data, batch_size=72, shuffle=True)
-   validate_loader = torch.utils.data.DataLoader(validate_data, batch_size=419, shuffle=False)
-   test_loader = torch.utils.data.DataLoader(test_data, batch_size=273, shuffle=False)
+   train_loader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)  # 1,088.
+   validate_loader = torch.utils.data.DataLoader(validate_data, batch_size=68)         # 136.
+   test_loader = torch.utils.data.DataLoader(test_data, batch_size=128)                 # 136.
 
    model = AlexNet().to(device)
 
    optimizer = optim.Adam(model.parameters(), lr=0.001)
-   scheduler = StepLR(optimizer, step_size=1, gamma=gamma)
 
-   for epoch in range(1, epochs+1):
-      train(model, device, train_loader, validate_loader, optimizer, epoch)
-      valid_test(model, device, test_loader, 0)
-      scheduler.step()
+   # for epoch in range(1, epochs+1):
+   #    train(model, device, train_loader, validate_loader, optimizer, epoch)
+   #    valid_test(model, device, test_loader, 0)
 
 if __name__ == '__main__':
    main()
