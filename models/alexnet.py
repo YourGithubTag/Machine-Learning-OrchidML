b6046e64f5eaa5aace742f448035cb8caeef0656
Jonathan Thamrin <jonathanthamrin@Jonathans-MacBook-Pro.local>
2020-05-08 11:12:14 +1200

    FINAL MODELS

---
+++ models/alexnet.py
@@ -0,0 +1,55 @@
+# The AlexNet implementation below is identical to the published paper.
+# All layers and parameters used have been referenced to their appropriate sections of the report.
+# Note: Identical layers have not been commented due to similarities with other comments.
+
+# Link: https://papers.nips.cc/paper/4824-imagenet-classification-with-deep-convolutional-neural-networks.pdf
+
+import torch
+import torch.nn as nn
+
+class AlexNet(nn.Module):
+
+   def __init__(self):
+      super(AlexNet, self).__init__()
+      # Section 3.5 - Overall Architecture (Convolutional Layers + Max Pooling)
+      self.features = nn.Sequential(
+         nn.Conv2d(in_channels=3, out_channels=96, kernel_size=11, stride=4),
+         nn.ReLU(),
+         # Section 3.3 - Local Response Normalization
+         nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2),           
+         nn.MaxPool2d(kernel_size=3, stride=2), 
+         nn.Conv2d(96, 256, 5, padding=2),  
+         nn.ReLU(),
+
+         nn.LocalResponseNorm(5, 0.0001, 0.75, 2),
+         nn.MaxPool2d(3, 2),
+         nn.Conv2d(256, 384, 3, padding=1),  
+         nn.ReLU(),
+
+         nn.Conv2d(384, 384, 3, padding=1),  
+         nn.ReLU(),
+
+         nn.Conv2d(384, 256, 3, padding=1), 
+         nn.ReLU(),
+
+         nn.MaxPool2d(3, 2)
+      )
+      
+      self.classifier = nn.Sequential(
+         # Section 4.2 - Dropout
+         nn.Dropout(p=0.5, inplace=True),
+         # Section 3.5 - Linear Layers
+         nn.Linear(in_features=(256 * 6 * 6), out_features=4096),
+         nn.ReLU(),
+         nn.Dropout(0.5, True),
+         nn.Linear(4096, 4096),
+         nn.ReLU(),
+         nn.Linear(4096, 17)
+      )
+   
+   # Forward() links all the layers together.
+   def forward(self, x):
+      x = self.features(x)
+      x = torch.flatten(x, 1)
+      x = self.classifier(x)
+      return x
---
+++ models/vgg.py
@@ -0,0 +1,102 @@
+# The VGG-16 implementation below is identical to the published paper with minor tweaks.
+# All layers and parameters used have been referenced to their appropriate sections of the report.
+# Weight initialisation has been adapted from PyTorch.
+# Note: Identical layers have not been commented due to similarities with other comments.
+
+# Link: https://arxiv.org/pdf/1409.1556.pdf
+
+import torch
+import torch.nn as nn
+
+class VGG16(nn.Module):
+
+    def __init__(self):
+        super(VGG16, self).__init__()
+        # Section 2.2 - Configurations, Table 1 (Convolutional Layers)
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            # Added Batch Normalisation to improve accuracy (NOT in official paper)
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            # Section 2.1 - Architecture (Max Pooling)
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(64, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(128, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(256, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+        )
+        # Section A.2 - Localisation Experiments (Average Pooling)
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+
+        # Section 2.3 - Discussions
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 17),
+        )
+
+        self._initialize_weights()
+    
+    # Forward() links all the layers together
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
+    # This initializes the weights (hidden layers) based on specifications from the official paper
+    # Adapted from PyTorch GitHub
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
