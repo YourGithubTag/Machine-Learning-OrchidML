4252467eeb8e9dfcefbcf7851152861066e0f3cc
jthamrin-dev <jthamrin.dev@gmail.com>
2020-05-08 22:43:59 +1200

    converted resnet to improved vgg

--- models/vgg_v2.py
+++ models/vgg_v2.py
@@ -3,7 +3,8 @@
 DISCLAIMER: ALL CODE IS WRITTEN FROM SCRATCH AND MAY CONTAIN SIMILARITIES TO CODE FOUND ONLINE.
             APPROPRIATE ACKNOWLEDGEMENTS HAVE BEEN MADE WHERE NECESSARY.
 
-The ResNet-18 implementation below is identical to the published paper.
+This model is a more advanced version of VGG-16 and is based on the Plain Network proposed in the published paper.
+It has identical features with ResNet-18 however does not feature shortcut connections (i.e. NOT residual).
 All layers and parameters used have been referenced to their appropriate sections of the report.
 Note: Identical layers have not been commented due to similarities with other comments.
 
@@ -14,17 +15,20 @@ Link: https://arxiv.org/pdf/1512.03385.pdf
 import torch
 import torch.nn as nn
 
-class ResNet18(nn.Module):
+class VGG_v2(nn.Module):
 
     def __init__(self):
-        super(ResNet18, self).__init__()
-        # Section 4 - Experiments, Table 1 (Convolutional Layers, Max Pooling)
+        super(VGG_v2, self).__init__()
+         (Convolutional Layers, Max Pooling)
         self.features = nn.Sequential(
             # Conv. Layer 1
+                # 2D Convolution based on Section 4 - Experiments, Table 1.
             nn.Conv2d(3, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False),
-            # Section 3.4 - Implementation (Batch Normalisation, RELU)
-            nn.BatchNorm2d(64),
+                # Batch Normalisation based on Section 3.4 - Implementation.
+            nn.BatchNorm2d(num_features=64),
+                # ReLU Activation based on based on Section 3.4 - Implementation.
             nn.ReLU(inplace=True),
+                # Max Pooling based on Section 4 - Experiments, Table 1.
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1, dilation=1, ceil_mode=False),
 
             # Conv. Layer 2
@@ -76,15 +80,15 @@ class ResNet18(nn.Module):
             nn.BatchNorm2d(512)
         )
 
-        # Section 3.3 - Network Architectures (Average Pooling)
+        # Average Pooling based on Section 3.3 - Network Architectures.
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
 
-        # Section 3.3 - Network Architectures (FC Layer)
+        # Linear Transformation based on Section 3.3 - Network Architectures.
         self.classifier = nn.Sequential(
             nn.Linear(in_features=512, out_features=17, bias=True)
         )
     
-    # Forward() links all the layers together.
+    # Forward() links all the layers together based on Section 4 - Experiments, Table 1.
     def forward(self, x):
         x = self.features(x)
         x = self.avgpool(x)
