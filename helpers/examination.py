accd9d7bb7d89895131fb894aa363bce952b8f0e
Jonathan Thamrin <jonathanthamrin@Jonathans-MacBook-Pro.local>
2020-05-09 15:30:02 +1200

    comments added to helpers

--- helpers/examination.py
+++ helpers/examination.py
@@ -5,6 +5,8 @@ from sklearn import metrics
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import ConfusionMatrixDisplay
 
+# Function which collects and returns the images used for testing,
+# their labels and the predictions (probabilities).
 def get_predictions(model, iterator, device):
     model.eval()
     images = []; labels = []; probs = []
@@ -15,18 +17,20 @@ def get_predictions(model, iterator, device):
             y_predict = model(x)
             y_probability = F.softmax(y_predict, dim=-1)
             top_pred = y_probability.argmax(1, keepdim=True)
-
+            # Adds to the lists (using the CPU to reduce strain on GPU memory).
             images.append(x.cpu())
             labels.append(y.cpu())
             probs.append(y_probability.cpu())
 
+    # Concatenates all tensors from each respective list.
     images = torch.cat(images, dim =0)
     labels = torch.cat(labels, dim=0)
     probs = torch.cat(probs, dim=0)
 
     return images, labels, probs
 
-
+# Plots the confusion matrix using SciLearn Kit. Interaction with plots is deactivated
+# and is saved in root with their respective model names.
 def plot_confusion_matrix(labels, pred_labels, classes, modelname):
     plt.ioff()
     fig = plt.figure(figsize = (17, 17))
@@ -39,8 +43,11 @@ def plot_confusion_matrix(labels, pred_labels, classes, modelname):
     plt.savefig(figfilename)
     plt.close(fig)
 
+# Generates the results for precision rate, recall rate and f1 rate.
+# Provides accuracy results as well using SciLearn Kit.
 def class_report(pred_labels, data, dp):
-
+    
+    # Extracts all correct labels from the data_loader.
     t_images, t_labels = zip(*[(image, label) for image, label in
                                [data[i] for i in range(len(data))]]) 
     t_labels = [i for i in t_labels]
--- helpers/helpers.py
+++ helpers/helpers.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 
-
+# This function plots the graphs for training, validation and testing.
+# Interactive mode is disabled and all graphs are saved to the root directory with appropriate names.
 def plot_graphs_csv(x, y, y_label, label, modelname):
     plt.ioff()
     plt.figure()
@@ -14,14 +15,16 @@ def plot_graphs_csv(x, y, y_label, label, modelname):
     plt.savefig(figfilename)
     plt.close()
 
+    # Creates a CSV of results for the model - contains all data as shown in graphs.
     with open(csvfilename, 'a+', newline='') as file:
         wr = csv.writer(file)
         wr.writerow(y_label)
         wr.writerow(x)
         wr.writerow(y)
 
-def calculate_accuracy(y_pred, y):
-    top_pred = y_pred.argmax(1, keepdim = True)
-    correct = top_pred.eq(y.view_as(top_pred)).sum()
-    acc = correct.float() / y.shape[0]
+# Calculates the accuracy by comparing number of correct and incorrect predictions.
+def calculate_accuracy(pred, actual):
+    top_pred = pred.argmax(1, keepdim = True)
+    correct = top_pred.eq(actual.view_as(top_pred)).sum()
+    acc = correct.float() / actual.shape[0]
     return acc
