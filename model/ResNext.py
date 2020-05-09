b92bac96c5f514e92c66612ddd5fa2e9d210a9da
Nikhil <nikhil@pop-os.localdomain>
2020-05-09 12:55:05 +1200

    Added References to code, more commenting

--- model/ResNext.py
+++ model/ResNext.py
@@ -3,9 +3,16 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-# This function generates the "bottleneck" block that is used in ResNext. The bottleneck block is the building block which ResNext is built off  
+# This function generates the "bottleneck" block that is used in ResNext. The bottleneck block is the building block which ResNext as described in papers such as 
+# [Deep Residual Learning for Image Recognition. 10 Dec 2015, Kaiming He, et al]
+# [Aggregated Residual Transformations for Deep Neural Networks. 11 Apr 2017, Saining Xie, et al]
+
+# Stride in original bottleneck is placed in the Convolution with kernal size of 1 (1x1 conv) however as per 
+# [ResNet v1.5 for PyTorch, Nvidia Research] https://ngc.nvidia.com/catalog/model-scripts/nvidia:resnet_50_v1_5_for_pytorch
+# stride is placed in the convolution of kernel size = 3, improving accuracy as described by Nvidia
+
 class Bottleneck(nn.Module):
-        expansion = 4
+        expansion = 4 # This value determines by how much each layer increases their filter size by
         def __init__(self,inplanes, planes, stride=1, down=False ,groups=1, base_width=64):
             super(Bottleneck, self).__init__()
 
@@ -49,7 +56,9 @@ class Bottleneck(nn.Module):
             return out
 
 
-# This function constructs the ResNext network and also contains the forward pass in the macro scope of the network.
+# This function constructs the ResNext network and also contains the forward pass in the macro scope of the network, as described by
+# [Deep Residual Learning for Image Recognition. 10 Dec 2015, Kaiming He, et al] and 
+# [Aggregated Residual Transformations for Deep Neural Networks. 11 Apr 2017, Saining Xie, et al]
 # It uses the Bottleneck building block, which contains the Bottleneck architecture on which ResNext is built off.  
 class ResNext(nn.Module):
     def __init__(self,groups,width_per_group):
@@ -89,19 +98,22 @@ class ResNext(nn.Module):
     # Input sizes are calculated ahead of time, as given by the planeslist requirements. 
     # 
     def bottlenecklayer(self, stride=1):
+        # The bottleneck layers follow the predetermined (3, 4, 6, 3) architecture which has been described in the 
+        # Residual learning for image recognition paper for ResNext50
         return nn.Sequential(
-            # Layer 1
+
+            # Layer 1 - 3 blocks [input:64, output:256]
             Bottleneck(64, self.planeslist[0], stride, down=True, groups=self.groups,base_width=self.base_width),
             Bottleneck(256,self.planeslist[0], groups=self.groups,base_width=self.base_width),
             Bottleneck(256,self.planeslist[0], groups=self.groups,base_width=self.base_width),
 
-            # Layer 2
+            # Layer 2 - 4 blocks [input:256, output:512]
             Bottleneck(256, self.planeslist[1], stride=2,down=True, groups=self.groups,base_width=self.base_width),
             Bottleneck(512, self.planeslist[1], groups=self.groups,base_width=self.base_width),
             Bottleneck(512, self.planeslist[1], groups=self.groups,base_width=self.base_width),
             Bottleneck(512, self.planeslist[1], groups=self.groups,base_width=self.base_width),
             
-            # Layer 3
+            # Layer 3 - 6 blocks [input:512, output:624]
             Bottleneck(512, self.planeslist[2], stride=2, down=True, groups=self.groups,base_width=self.base_width),
             Bottleneck(624, self.planeslist[2], groups=self.groups,base_width=self.base_width),
             Bottleneck(624, self.planeslist[2], groups=self.groups,base_width=self.base_width),
@@ -109,7 +121,7 @@ class ResNext(nn.Module):
             Bottleneck(624, self.planeslist[2], groups=self.groups,base_width=self.base_width),
             Bottleneck(624, self.planeslist[2], groups=self.groups,base_width=self.base_width),
 
-            # Layer 4
+            # Layer 4 - 3 blocks [input:624, output:2048]
             Bottleneck(624, self.planeslist[3], stride=2,down=True, groups=self.groups,base_width=self.base_width),
             Bottleneck(2048,self.planeslist[3], groups=self.groups,base_width=self.base_width),
             Bottleneck(2048,self.planeslist[3], groups=self.groups,base_width=self.base_width))
